547d07602af6c23bd421820112d84cfeb2bd563f
Ben Mares <services-git-throwaway1@tensorial.com>
2024-10-31 15:10:10 +0100

    Remove 'strip' from Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,8 +4,10 @@ cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 :: build
 cargo install --locked --root "%LIBRARY_PREFIX%" --path "crates\resvg" || goto :error
 
-:: strip debug symbols
-strip "%LIBRARY_PREFIX%\bin\resvg.exe" || goto :error
+:: :: I had to disable this because it was causing a hang.
+:: :: <https://matrix.to/#/!SOyumkgPRWoXfQYIFH:matrix.org/$17188919988GjPNB:gitter.im?via=matrix.org&via=gitter.im&via=ifisc.uib-csic.es>
+:: :: strip debug symbols
+:: strip "%LIBRARY_PREFIX%\bin\resvg.exe" || goto :error
 
 :: remove extra build file
 del /F /Q "%LIBRARY_PREFIX%\.crates.toml"
